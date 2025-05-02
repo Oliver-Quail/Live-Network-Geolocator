30cec6b68d02c037e3f0e5147e3a85970dc6dc53
Ollie-Q <82865751+Oliver-Quail@users.noreply.github.com>
2025-05-02 10:08:31 +1000

    Added geolcated function to program

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pyshark
 import re
 from CustomThread import CustomThread
+import pygeoip
+
+geo_data = pygeoip.GeoIP("GeoLiteCity.dat")
 
 capture = pyshark.FileCapture("./samples/evidence01.pcap", keep_packets=False)
 
@@ -15,7 +18,7 @@ for packet in capture:
     except AttributeError:
         print("Packet contained no IP addres")
         continue
-    
+    # Filter out all packets will local ip addresses
     if re.match("(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)", packet_source_ip):
         if packet_source_ip not in local_ips:
             local_ips.append(packet_source_ip)
@@ -26,5 +29,16 @@ for packet in capture:
 
 print(local_ips)
 print(extenarl_ips)
-#https://medium.com/@angry_programmer/capturing-network-packets-using-pyshark-d44b6a1b049d
+
+
+def getLocation(ip, location_data):
+    ip_data = location_data.record_by_name(ip)
+    ip_logitude = ip_data["longitude"]
+    ip_latitude = ip_data["latitude"]
+    ip_city = ip_data["city"]
+    print("Ip address location: (%3f, %3f)", (ip_latitude, ip_logitude) )
+    pass
+
+
+getLocation(extenarl_ips[0], geo_data)
 
